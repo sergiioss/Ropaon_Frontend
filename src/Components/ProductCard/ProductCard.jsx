3f6977b2d45ab5693fb87565af57135f27b5d3b4
sergiioss <caos13es@hotmail.com>
2022-08-23 11:19:30 +0200

    Fix bug Home

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -2,25 +2,14 @@ import React from "react";
 import './ProductCard.css'
 
 const ProductCard = props => {
-    
+
     return (
         <div className="Productcard">
             <div className="product">
-                
-                
-                {
-                props.data
-                /* <div>
-                <strong className="ntitulo">{}</strong><br></br>;
-                </div> 
-                <strong className="ntitulo">{props.data.products.name}</strong><br></br>
-                <br></br><img className="products" src={props.data.name}></img><br></br>
-                <strong>Precio:</strong> {props.data.size}<br></br> 
-                <strong>Color:</strong> {props.data.product_price}
-                <strong>Color:</strong> {props.data.color} 
-                <strong>Color:</strong> {props.data.gender}
-                <strong>Color:</strong> {props.data.url}  */}
-                    
+                <h1>{props.data.name}</h1>
+                <img className="foto" src={props.data.url}/>
+                <h3>Talla: {props.data.size}</h3>
+                <h3>Color: {props.data.color}</h3>
             </div>
         </div>
     )
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -9,31 +9,28 @@ import ProductCard from "../../Components/ProductCard/ProductCard"
 const Home = (props) => {
 
     const [products, setProducts] = useState({
-        prod: ""
+        prod: []
     })
-        console.log(products.prod)
+
     useEffect(() => {
         axios.get('https://ropaon.herokuapp.com/api/productall')
             .then(resp => {
                 setProducts({
-                    prod: [resp.data.data]
+                    prod: resp.data.data
                 })
-                /* const pal = products.prod[0].data */
-                console.log(resp.data.data)
-                console.log('hola')
             })
 
     }, [])
-    
-    
+
+
     return (
         <div className="home">
             <div>
-                
+
                 {
-                    products.prod[0].map((index, prod)=> (
-                        <ProductCard key={index} data={prod}/>
-                    ))
+                    products.prod.map((prod) =>
+                        <ProductCard data={prod} />
+                    )
                 }
             </div>
         </div>
