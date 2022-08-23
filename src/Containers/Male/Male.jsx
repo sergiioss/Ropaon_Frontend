f66b1e2039f85fa1ec7ea4bf44686828947a1a9b
sergiioss <caos13es@hotmail.com>
2022-08-23 13:59:11 +0200

    Vista hombre empezado

--- src/Containers/Male/Male.jsx
+++ src/Containers/Male/Male.jsx
@@ -1,13 +1,37 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import "./Male.css"
 import axios from "axios"
+import {Container, Row, Col} from 'react-bootstrap'
+import ProductCard from "../../Components/ProductCard/ProductCard"
 
 const Male = (props) => {
+    const [productMale, setProductMale] = useState({
+        male: []
+    })
 
-    return(
-        <div className="male">
-            <h1>Vista male</h1>
-        </div>
+    useEffect(() => {
+        axios.get('https://ropaon.herokuapp.com/api/productgenderm')
+            .then(resp => {
+                setProductMale({
+                    male: resp.data.message
+                })
+                
+            })
+
+    }, [])
+
+    return (
+        <Container className="home">
+            <Row>
+                {
+                    productMale.male.map((male, index) => (
+                        <Col key={index} xs={6} sm={6} md={4} xl={2}>
+                            <ProductCard data={male} key={index} />
+                        </Col>
+                    ))
+                }
+            </Row>
+        </Container>
     )
 }
 
