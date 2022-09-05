9e862b495329861adac31e1f522ff92e0dbeb542
sergiioss <caos13es@hotmail.com>
2022-09-06 01:50:46 +0200

    Añadido 3 endpoints en la vista Admin para hacer un crud de productos

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -3,22 +3,31 @@ import './ProductCard.css'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { Button } from "react-bootstrap";
+import { Button, NavLink } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { selectDatosUsuario } from "../../Containers/User/userSlice"
 import { arrayPurchase } from "../../Containers/Purchase/purchaseSlice";
 import { useState } from "react";
+import { idProduct } from "../../Containers/Admin/adminSlice";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = props => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const credenciales = useSelector(selectDatosUsuario);
 
     let [evento, setEvento] = useState([])
 
-    const deleteItem = (event) => (dispatch) =>{
+    const deleteItem = (event) => (dispatch) => {
         setEvento(event)
     }
-    
+    const updateProduct = (event) => (dispatch) => {
+        console.log(event.name);
+        console.log(event.id);
+        dispatch(idProduct(event));
+        navigate("/admin");
+    }
+
     if (!credenciales?.token) {
         return (
             <Row xs={1} md={3} className="g-4 img-fluid">
@@ -44,7 +53,34 @@ const ProductCard = props => {
             </Row>
 
         );
-    }else if(props.data.id !== evento){
+    } else if (credenciales.user.id === 7) {
+        return (
+            <Row xs={1} md={3} className="g-4 img-fluid">
+                {Array.from({ length: 1 }).map((_, idx) => (
+                    <Col key={idx} className="card-cont">
+                        <Card className="card-global">
+                            <Card.Body className="card">
+                                <Card.Title>{props.data.name}</Card.Title>
+                                <Card.Img className="foto" src={props.data.url} />
+                                <Card.Text>
+                                    Talla: {props.data.size}
+                                </Card.Text>
+                                <Card.Text>
+                                    Color: {props.data.color}
+                                </Card.Text>
+                                <Card.Text>
+                                    Precio: {props.data.product_price},99 €
+                                </Card.Text>
+                                {props.buy != 1 && <Button onClick={() => { dispatch(updateProduct(props.data)) }}>
+                                    Seleccionar
+                                </Button>}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        )
+    } else if (props.data.id !== evento) {
         return (
             <Row xs={1} md={3} className="g-4 img-fluid">
                 {Array.from({ length: 1 }).map((_, idx) => (
@@ -75,7 +111,6 @@ const ProductCard = props => {
             </Row>
         );
     }
-
 }
 
 export default ProductCard
--- src/Containers/Admin/adminSlice.js
+++ src/Containers/Admin/adminSlice.js
@@ -3,26 +3,42 @@ import axios from 'axios';
 
 export const adminSlice = createSlice({
     name: 'product',
-    initialState:{
+    initialState: {
         name: "",
         size: "",
-        product_price:"",
-        url:"",
-        color:"",
-        gender:"",
+        product_price: "",
+        url: "",
+        color: "",
+        gender: "",
+        id: ""
     },
-    reducers:{
+    reducers: {
         registerProduct: (state, action) => {
             return {
                 ...state,
                 ...action.payload
             }
         },
+        newId: (state, action) => {
+            state.id = action.payload
+            state.name = action.payload
+        },
     },
 });
 
+export const idProduct = (event) => async (dispatch) => {
+    try {
+        dispatch(newId({
+            id: event.id,
+            name: event.name
+        }))
 
-export const {registerProduct} = adminSlice.actions
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const { registerProduct, newId } = adminSlice.actions
 
 export const selectProduct = (state) => state.product
 
