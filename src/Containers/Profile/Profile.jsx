a06ada46ae293ced35cda5494b02a2c3c5e6dc70
sergiioss <sergiiptx@gmail.com>
2023-01-20 02:50:23 +0100

    Photo

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import "./Profile.css"
 import { useSelector, useDispatch } from 'react-redux';
-import { selectDatosUsuario} from '../User/userSlice';
-import { logOut} from '../User/userSlice'
+import { selectDatosUsuario } from '../User/userSlice';
+import { logOut } from '../User/userSlice'
 import { useNavigate } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
-import Stack from 'react-bootstrap/Stack'
+import { Container, Row, Col } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
 
 
 const Profile = props => {
@@ -13,27 +13,36 @@ const Profile = props => {
     const credenciales = useSelector(selectDatosUsuario)
     const dispatch = useDispatch()
     const navegador = useNavigate()
+    const photo = credenciales.user.photo
+    console.log(credenciales)
 
     return (
-        <div className="profile">
-            <Stack gap={4} className="col-md-5 mx-auto">
-                <Button> Save Changes </Button>
-                <Button> Cancel </Button>
-            </Stack>
-            <p>BIENVENIDO! </p>
-            <p>Nombre:{credenciales.user.name}</p><br></br>
-            <p>E-mail:{credenciales.user.email}</p><br></br>
-            <p>Telefono:{credenciales.user.addres}</p>
+        <Container className="profile">
+            <Row>
+                <Col className="photo">
+                    <Card.Img src={credenciales.user.photo}></Card.Img>
+                </Col>
+                <Col className="information">
+                    <p>BIENVENIDO! </p>
+                    <p>Nombre:{credenciales.user.name}</p><br></br>
+                    <p>E-mail:{credenciales.user.email}</p><br></br>
+                    <p>Telefono:{credenciales.user.addres}</p>
+                </Col>
+            </Row>
+
 
             <div>
-                <div className="sendButton" onClick={()=>{
-                        /* navegador("/") */
-                         /* localStorage.clear() */ 
-                        dispatch(logOut())
-                        navegador("/")
-                    }}>Logout</div><br></br>
+                <div className="perfil">
+
+                </div>
+                <div className="sendButton" onClick={() => {
+                    /* navegador("/") */
+                    /* localStorage.clear() */
+                    dispatch(logOut())
+                    navegador("/")
+                }}>Logout</div><br></br>
             </div>
-        </div>
+        </Container>
     )
 }
 
