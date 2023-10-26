5259ee707458c1ad4a27743603657e4e612d0b4d
sergiioss <sergiiptx@gmail.com>
2023-10-26 23:01:22 +0200

    velocidad en la carga de paginas que ya se han cargado por primera vez

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -8,31 +8,60 @@ import ProductCard from "../../Components/ProductCard/ProductCard"
 const Home = (props) => {
 
     const [products, setProducts] = useState({
-        prod: []
+        newData:[]
     })
-
+    /* useEffect(() => {
+        axios.get('https://sergi.lighthousemarketing.es/public/api/productall''http://localhost:8000/api/productall')
+        .then(resp => {
+            setProducts({
+                newData: resp.data.data
+            })
+        })   
+        
+    }, []) */
+    
     useEffect(() => {
-        axios.get('https://sergi.lighthousemarketing.es/public/api/productall'/* 'http://localhost:8000/api/productall' */)
+        const cachedData = localStorage.getItem('storage');
+        // Intenta cargar los datos desde el almacenamiento local
+        console.log(cachedData !== "string")
+        if (cachedData) {
+            return;
+        } else {
+            console.log("hola")
+          // Si no hay datos en la caché, realiza una carga inicial y guárdalos en la caché
+          axios.get(/* 'https://sergi.lighthousemarketing.es/public/api/productall' */'http://localhost:8000/api/productall')
             .then(resp => {
                 setProducts({
-                    prod: resp.data.data
+                    newData: resp.data.data
                 })
-            })
+                localStorage.setItem('storage', JSON.stringify(resp.data.data));
+            })  
+        }
+      }, []);
 
-    }, [])
-
-
+      const dataObj = JSON.parse(localStorage.getItem('storage')); 
+  
     return (
 
     <Container className="home">
         <Row>
+
             {
-                products.prod.map((prod, i) => (
+                !localStorage.getItem('storage') &&
+                products.newData.map((prod, i) => (
                     <Col key={i} xs={6} sm={6} md={4} xl={2} className="img-fluid minw">
                         <ProductCard data={prod} key={i}/>
                     </Col>
                 ))
             }
+            {
+                localStorage.getItem('storage') &&
+                dataObj.map((prod, i)=>(
+                    <Col key={i} xs={6} sm={6} md={4} xl={2} className="img-fluid minw">
+                        <ProductCard data={prod} key={i}/>
+                    </Col>
+                ))
+            }            
         </Row>
     </Container>
     )
